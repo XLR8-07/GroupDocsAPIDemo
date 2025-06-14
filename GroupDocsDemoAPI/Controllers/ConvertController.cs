1a8ee531a156c05c4a5f8c44f746c6d73535a9ce
XLR8-07 <joyashikur@gmail.com>
2025-06-14 21:54:53 +0200

    [API Implementation] Get Possible conversion types

--- GroupDocsDemoAPI/Controllers/ConvertController.cs
+++ GroupDocsDemoAPI/Controllers/ConvertController.cs
@@ -1,4 +1,5 @@
 using GroupDocs.Conversion;
+using GroupDocs.Conversion.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using GroupDocs.Conversion.Options.Convert;
 using GroupDocsDemoAPI.Services;
@@ -11,6 +12,19 @@ public class ConvertController: ControllerBase
 {   
     // private readonly ConversionService conversionService;
     private readonly FileUploadService fileUploadService = new FileUploadService();
+
+    [HttpPost("formats")]
+    public async Task<IActionResult> getConversionTypes([FromForm] IFormFile file)
+    {
+        string uploadedRelativePath = await fileUploadService.UploadFile(file);
+        
+        using (Converter converter = new Converter(uploadedRelativePath))
+        {
+            PossibleConversions possibleConversions = converter.GetPossibleConversions();
+            Console.WriteLine("The source document is of type {0} and could be converted to:", possibleConversions.Source.Extension);
+            return Ok(possibleConversions.All);
+        }
+    }
     
     [HttpPost("word/to/{type}")]
     public async Task<IActionResult> WordConvertToFormat([FromRoute] string type, [FromForm] IFormFile file)
@@ -29,11 +43,22 @@ public class ConvertController: ControllerBase
         return File(resultBytes, "application/octet-stream", outputPath);
     }
 
-    // [HttpPost("pdf/to/{type}")]
-    // public IActionResult PDFConvertToFormat([FromRoute] string type, [FromForm] IFormFile file)
-    // {
-    //     
-    // }
+    [HttpPost("pdf/to/{type}")]
+    public async  Task<IActionResult> PDFConvertToFormat([FromRoute] string type, [FromForm] IFormFile file)
+    {
+        string uploadedRelativePath = await fileUploadService.UploadFile(file);
+        string outputPath = fileUploadService.getFileOutputPath(file.FileName, type);
+        
+        using (var converter = new Converter(uploadedRelativePath))
+        {
+            var saveOptions = GetConvertOptions(type);
+            Console.WriteLine($"Converting {type} to {outputPath}");
+            converter.Convert(outputPath , saveOptions);
+        }
+
+        var resultBytes = System.IO.File.ReadAllBytes(outputPath);
+        return File(resultBytes, "application/octet-stream", outputPath);
+    }
     
     private static ConvertOptions GetConvertOptions(string format)
     {

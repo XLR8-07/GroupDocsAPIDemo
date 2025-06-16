29581c7cfa6c4e6aee9e9a799582caf1e5e3e5f4
XLR8-07 <joyashikur@gmail.com>
2025-06-16 09:00:11 +0200

    [API] Signature APIs

--- GroupDocsDemoAPI/Controllers/SignatureController.cs
+++ GroupDocsDemoAPI/Controllers/SignatureController.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using GroupDocs.Signature;
 using GroupDocs.Signature.Domain;
 using GroupDocs.Signature.Options;
@@ -33,24 +34,43 @@ public class SignatureController: ControllerBase
     }
     
     [HttpPost("addTextSignature")]
-    public async Task<IActionResult> AddImageSignature([FromForm] IFormFile file, [FromForm] string sign)
+    public async Task<IActionResult> AddTextSignature([FromForm] IFormFile file)
     {
         string uploadedRelativePath = await fileUploadService.UploadFile(file);
         
         using (Signature signature = new Signature(uploadedRelativePath))
         {
-            TextSignOptions options = new TextSignOptions("John Smith")
+            TextSignOptions options = new TextSignOptions("iBAS++")
             {
                 // set signature position
                 Left = 500,
-                Top = 6500,
+                Top = 650,
                 // set signature rectangle
                 Width = 100,
                 Height = 30,
-                Font = new SignatureFont { Size = 16, FamilyName = "Comic Sans MS" }
+                Font = new SignatureFont { Size = 24, FamilyName = "Comic Sans MS" },
+                ForeColor = Color.Brown
             };
             signature.Sign("TextSigned.pdf", options);
             return Ok("Signed Successfully, Check TextSigned.pdf");
+        }
+    }
+    
+    [HttpPost("addBarCodeSignature")]
+    public async Task<IActionResult> AddBarcodeSignature([FromForm] IFormFile file)
+    {
+        string uploadedRelativePath = await fileUploadService.UploadFile(file);
+        
+        using (Signature signature = new Signature(uploadedRelativePath))
+        {
+            BarcodeSignOptions options = new BarcodeSignOptions("iBAS++")
+            {
+                EncodeType = BarcodeTypes.Code128,
+                Left = 100,
+                Top = 100
+            };
+            signature.Sign("BarcodeSigned.pdf", options);
+            return Ok("Signed Successfully, Check BarcodeSigned.pdf");
         }
     }
 }

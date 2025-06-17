92f7fcf59ca1115e30b62f01414cd34d3b1159e7
XLR8-07 <joyashikur@gmail.com>
2025-06-17 11:16:33 +0200

    [API] Exact Text Redaction

---
+++ GroupDocsDemoAPI/Controllers/RedactionController.cs
@@ -0,0 +1,29 @@
+using GroupDocs.Redaction;
+using GroupDocs.Redaction.Redactions;
+using GroupDocsDemoAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupDocsDemoAPI.Controllers;
+
+[Route("api/[controller]")]
+public class RedactionController : ControllerBase
+{
+    private readonly FileUploadService fileUploadService = new FileUploadService();
+    
+    [HttpPost("hideText")]
+    public async Task<IActionResult> HideText([FromForm] IFormFile file)
+    {
+        string uploadFilePath = await fileUploadService.UploadFile(file);
+
+        using (Redactor redactor = new Redactor(uploadFilePath))
+        {
+            RedactorChangeLog result =
+                redactor.Apply(new ExactPhraseRedaction("QUICK BROWN FOX", new ReplacementOptions("[Sensitive]")));
+            if (result.Status != RedactionStatus.Failed)
+            {
+                redactor.Save();
+            }
+            return Ok(result.Status);
+        }
+    }
+}
